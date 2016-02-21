0d44bb0bf94df0022e89b64e62e3711d30ecc224
mmonkey <mmonkey@gmail.com>
2016-02-21 14:34:24 -0600

    InteractWithBlock LIstener

--- src/main/java/com/github/mmonkey/Automator/Listeners/InteractWithBlock.java
+++ src/main/java/com/github/mmonkey/Automator/Listeners/InteractWithBlock.java
@@ -2,7 +2,10 @@ package com.github.mmonkey.Automator.Listeners;
 
 import com.github.mmonkey.Automator.DefaultToolMapping;
 import org.spongepowered.api.block.BlockSnapshot;
+import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.living.player.gamemode.GameMode;
+import org.spongepowered.api.entity.living.player.gamemode.GameModes;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.InteractBlockEvent;
 import org.spongepowered.api.item.ItemType;
@@ -10,6 +13,7 @@ import org.spongepowered.api.item.inventory.Inventory;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.Slot;
 import org.spongepowered.api.item.inventory.entity.Hotbar;
+import org.spongepowered.api.item.inventory.property.SlotIndex;
 import org.spongepowered.api.item.inventory.type.GridInventory;
 import org.spongepowered.api.text.Text;
 
@@ -27,11 +31,15 @@ public class InteractWithBlock {
         }
 
         Player player = optionalPlayer.get();
+        Optional<GameMode> gameMode = player.getGameModeData().get(Keys.GAME_MODE);
+
+        if (gameMode.isPresent() && (gameMode.get()).equals(GameModes.CREATIVE)) {
+            return;
+        }
+
         Optional<ItemStack> itemInHand = player.getItemInHand();
         BlockSnapshot blockSnapshot = event.getTargetBlock();
         ItemType[] defaultMappings = DefaultToolMapping.getBlockToolDefaults(blockSnapshot.getState().getType());
-
-        player.sendMessage(Text.of("You clicked a " + blockSnapshot.getState().getType().getName() + "."));
 
         // No tool found for this block
         if (defaultMappings == null) {
@@ -47,31 +55,97 @@ public class InteractWithBlock {
             }
         }
 
+        Slot activeHotbarSlot = null;
+        Inventory hotbarInventory = player.getInventory().query(Hotbar.class);
+        if (hotbarInventory instanceof Hotbar) {
+            Hotbar hotbar = (Hotbar) hotbarInventory;
+            int activeIndex = hotbar.getSelectedSlotIndex();
+            activeHotbarSlot = getActiveHotbarSlot(hotbar, activeIndex);
+
+            // If there is a compatible tool already in the hotbar, set selected index to that item's slot index
+            int compatibleIndex = getCompatibleToolHotbarIndex(hotbar, defaultMappings);
+            if (compatibleIndex >= 0 && compatibleIndex <= 8) {
+                hotbar.setSelectedSlotIndex(compatibleIndex);
+                return;
+            }
+
+        }
+
         // Search the players grid inventory
-        Inventory gridInventory = player.getInventory().query(GridInventory.class);
-        if (gridInventory instanceof GridInventory) {
-            GridInventory grid = (GridInventory) gridInventory;
+        Inventory grid = player.getInventory().query(GridInventory.class);
+        if (grid instanceof GridInventory) {
             Iterable<Slot> gridSlots = grid.slots();
             for (Slot slot : gridSlots) {
                 for (ItemType tool : defaultMappings) {
                     if (slot.contains(tool)) {
-                        player.sendMessage(Text.of("Grid contains " + tool.getType().getName()));
+                        swapItemInHand(player, activeHotbarSlot, slot);
+                        return;
                     }
                 }
             }
         }
 
-        // Search the players hotbar
-        Inventory hotBarInventory = player.getInventory().query(Hotbar.class);
-        if (hotBarInventory instanceof Hotbar) {
-            Hotbar hotBar = (Hotbar) hotBarInventory;
-            for (ItemType tool : defaultMappings) {
-                if (hotBar.contains(tool)) {
-                    player.sendMessage(Text.of("Hotbar contains " + tool.getType().getName()));
+    }
+
+    private Slot getActiveHotbarSlot(Hotbar hotbar, int activeIndex) {
+
+        Iterable<Slot> hotbarSlots = hotbar.slots();
+
+        int index = 0;
+        for (Slot slot : hotbarSlots) {
+
+            if (activeIndex == index) {
+                return slot;
+            }
+
+            index++;
+        }
+
+        return null;
+
+    }
+
+    /**
+     * Returns the index of the first tool matching the tools array in the Hotbar;
+     *
+     * @param hotbar Hotbar
+     * @param tools ItemType[]
+     * @return int
+     */
+    private int getCompatibleToolHotbarIndex(Hotbar hotbar, ItemType[] tools) {
+
+        for (int index = 0; index < hotbar.size(); index ++) {
+            Optional<Slot> slot = hotbar.getSlot(new SlotIndex(index));
+            if (slot.isPresent()) {
+                for (ItemType tool : tools) {
+                    if (slot.get().contains(tool)) {
+                        return index;
+                    }
                 }
             }
         }
 
-        // swap tools
+        return -1;
+
     }
+
+    private void swapItemInHand(Player player, Slot activeSlot, Slot toolSlot) {
+
+        ItemStack itemInHand = player.getItemInHand().isPresent() ? player.getItemInHand().get() : null;
+        ItemStack itemInSlot = toolSlot.peek().isPresent() ? toolSlot.poll().get() : null;
+
+        if (activeSlot != null) {
+            activeSlot.clear();
+        }
+
+        if (itemInSlot != null) {
+            toolSlot.clear();
+            player.setItemInHand(itemInSlot);
+        }
+
+        if (itemInHand != null) {
+            toolSlot.offer(itemInHand);
+        }
+    }
+
 }
